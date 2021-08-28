9061b27944b301193d5e5941c30aec15869b1ebe
F Jiang <fjiang2@github.com>
2021-08-27 21:49:52 -0500

    Moved project sqlentity to repo sqlcode

--- sqlentity/Entity/IQueryResultReader.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Sys.Data.Entity
-{
-    public interface IQueryResultReader
-    {
-        IEnumerable<TEntity> Read<TEntity>() where TEntity : class;
-    }
-}
--- syscore/Linq/Operation.cs
+++ syscore/Linq/Operation.cs
@@ -8,218 +8,188 @@ namespace Sys.Data
 {
 	public static class Operation
 	{
-        public static T IsNull<T>(this object value, T defaultValue)
-        {
-            if (value is T)
-                return (T)value;
+		public static void Insert(this DataTable dt, Action<DataRow> insert)
+		{
+			DataRow row = dt.NewRow();
+			insert(row);
+			dt.Rows.Add(row);
 
-            if (value == null || value == DBNull.Value)
-                return defaultValue;
+			dt.AcceptChanges();
+		}
 
-            throw new Exception($"{value} is not type of {typeof(T)}");
-        }
+		public static int Update(this DataTable dt, Action<DataRow> update)
+		{
+			return Update(dt, (_, row) => true, (_, row) => update(row));
+		}
 
-        public static T GetField<T>(this DataRow row, string columnName, T defaultValue = default(T))
-        {
-            if (!row.Table.Columns.Contains(columnName))
-                return defaultValue;
+		public static int Update(this DataTable dt, Func<DataRow, bool> where, Action<DataRow> update)
+		{
+			return Update(dt, (_, row) => where(row), (_, row) => update(row));
+		}
 
-            return IsNull<T>(row[columnName], defaultValue);
-        }
+		public static int Update(this DataTable dt, Action<int, DataRow> update)
+		{
+			return Update(dt, (_, row) => true, update);
+		}
 
-        public static void SetField(this DataRow row, string columnName, object value)
-        {
-            if (row.Table.Columns.Contains(columnName))
-            {
-                if (value == null)
-                    row[columnName] = DBNull.Value;
-                else
-                    row[columnName] = value;
-            }
-        }
+		public static int Update(this DataTable dt, Func<int, DataRow, bool> where, Action<int, DataRow> update)
+		{
+			int count = 0;
+			int i = 0;
+			foreach (DataRow row in dt.Rows)
+			{
+				if (where(i, row))
+				{
+					update(i, row);
+					count++;
+				}
+				i++;
+			}
 
-        public static void Insert(this DataTable dt, Action<DataRow> insert)
-        {
-            DataRow row = dt.NewRow();
-            insert(row);
-            dt.Rows.Add(row);
+			return count;
+		}
 
-            dt.AcceptChanges();
-        }
+		public static int Delete(this DataTable dt, Func<DataRow, bool> where)
+		{
+			return Delete(dt, (_, row) => where(row));
+		}
 
-        public static int Update(this DataTable dt, Action<DataRow> update)
-        {
-            return Update(dt, (_, row) => true, (_, row) => update(row));
-        }
+		public static int Delete(this DataTable dt, Func<int, DataRow, bool> where)
+		{
+			int count = 0;
+			int i = 0;
+			foreach (DataRow row in dt.Rows)
+			{
+				if (where(i, row))
+				{
+					row.Delete();
+					count++;
+				}
+				i++;
+			}
 
-        public static int Update(this DataTable dt, Func<DataRow, bool> where, Action<DataRow> update)
-        {
-            return Update(dt, (_, row) => where(row), (_, row) => update(row));
-        }
+			dt.AcceptChanges();
+			return count;
+		}
 
-        public static int Update(this DataTable dt, Action<int, DataRow> update)
-        {
-            return Update(dt, (_, row) => true, update);
-        }
+		public static DataTable ToDataTable<T>(this IEnumerable<T> source)
+		{
+			var properties = typeof(T).GetProperties();
 
-        public static int Update(this DataTable dt, Func<int, DataRow, bool> where, Action<int, DataRow> update)
-        {
-            int count = 0;
-            int i = 0;
-            foreach (DataRow row in dt.Rows)
-            {
-                if (where(i, row))
-                {
-                    update(i, row);
-                    count++;
-                }
-                i++;
-            }
+			DataTable dt = new DataTable();
+			foreach (var propertyInfo in properties)
+			{
+				dt.Columns.Add(new DataColumn(propertyInfo.Name, propertyInfo.PropertyType));
+			}
 
-            return count;
-        }
+			Func<T, object[]> selector = row =>
+			{
+				var values = new object[properties.Length];
+				int i = 0;
 
-        public static int Delete(this DataTable dt, Func<DataRow, bool> where)
-        {
-            return Delete(dt, (_, row) => where(row));
-        }
+				foreach (var propertyInfo in properties)
+				{
+					values[i++] = propertyInfo.GetValue(row);
+				}
 
-        public static int Delete(this DataTable dt, Func<int, DataRow, bool> where)
-        {
-            int count = 0;
-            int i = 0;
-            foreach (DataRow row in dt.Rows)
-            {
-                if (where(i, row))
-                {
-                    row.Delete();
-                    count++;
-                }
-                i++;
-            }
+				return values;
+			};
 
-            dt.AcceptChanges();
-            return count;
-        }
+			foreach (T row in source)
+			{
+				object[] values = selector(row);
+				var newRow = dt.NewRow();
+				int k = 0;
+				foreach (var item in values)
+				{
+					newRow[k++] = item;
+				}
 
-        public static DataTable ToDataTable<T>(this IEnumerable<T> source)
-        {
-            var properties = typeof(T).GetProperties();
+				dt.Rows.Add(newRow);
+			}
 
-            DataTable dt = new DataTable();
-            foreach (var propertyInfo in properties)
-            {
-                dt.Columns.Add(new DataColumn(propertyInfo.Name, propertyInfo.PropertyType));
-            }
+			dt.AcceptChanges();
+			return dt;
+		}
 
-            Func<T, object[]> selector = row =>
-            {
-                var values = new object[properties.Length];
-                int i = 0;
+		public static DataSet ToDataSet(this string xml)
+		{
+			DataSet ds = new DataSet();
+			return ToDataSet(xml, ds);
+		}
 
-                foreach (var propertyInfo in properties)
-                {
-                    values[i++] = propertyInfo.GetValue(row);
-                }
+		public static DataSet ToDataSet(this string xml, DataSet ds)
+		{
+			using (MemoryStream stream = new MemoryStream())
+			using (StreamWriter writer = new StreamWriter(stream))
+			{
+				writer.Write(xml);
+				writer.Flush();
+				stream.Position = 0;
 
-                return values;
-            };
-
-            foreach (T row in source)
-            {
-                object[] values = selector(row);
-                var newRow = dt.NewRow();
-                int k = 0;
-                foreach (var item in values)
-                {
-                    newRow[k++] = item;
-                }
-
-                dt.Rows.Add(newRow);
-            }
-
-            dt.AcceptChanges();
-            return dt;
-        }
-
-        public static DataSet ToDataSet(this string xml)
-        {
-            DataSet ds = new DataSet();
-            return ToDataSet(xml, ds);
-        }
-
-        public static DataSet ToDataSet(this string xml, DataSet ds)
-        {
-            using (MemoryStream stream = new MemoryStream())
-            using (StreamWriter writer = new StreamWriter(stream))
-            {
-                writer.Write(xml);
-                writer.Flush();
-                stream.Position = 0;
-
-                try
-                {
-                    ds.ReadXml(stream, XmlReadMode.ReadSchema);
-                }
-                catch (Exception)
-                {
-                    throw new Exception(xml);
-                }
-            }
-            return ds;
-        }
+				try
+				{
+					ds.ReadXml(stream, XmlReadMode.ReadSchema);
+				}
+				catch (Exception)
+				{
+					throw new Exception(xml);
+				}
+			}
+			return ds;
+		}
 
 
-        public static string ToXml(this DataSet ds)
-        {
-            using (MemoryStream stream = new MemoryStream())
-            {
-                ds.WriteXml(stream, XmlWriteMode.WriteSchema);
-                stream.Flush();
-                stream.Position = 0;
+		public static string ToXml(this DataSet ds)
+		{
+			using (MemoryStream stream = new MemoryStream())
+			{
+				ds.WriteXml(stream, XmlWriteMode.WriteSchema);
+				stream.Flush();
+				stream.Position = 0;
 
-                using (StreamReader reader = new StreamReader(stream))
-                {
-                    return reader.ReadToEnd();
-                }
-            }
-        }
+				using (StreamReader reader = new StreamReader(stream))
+				{
+					return reader.ReadToEnd();
+				}
+			}
+		}
 
-        public static DataTable ToDataTable(this string xml, DataTable dt)
-        {
-            using (MemoryStream stream = new MemoryStream())
-            using (StreamWriter writer = new StreamWriter(stream))
-            {
-                writer.Write(xml);
-                writer.Flush();
-                stream.Position = 0;
+		public static DataTable ToDataTable(this string xml, DataTable dt)
+		{
+			using (MemoryStream stream = new MemoryStream())
+			using (StreamWriter writer = new StreamWriter(stream))
+			{
+				writer.Write(xml);
+				writer.Flush();
+				stream.Position = 0;
 
-                try
-                {
-                    dt.ReadXml(stream);
-                }
-                catch (Exception)
-                {
-                    throw new Exception(xml);
-                }
-            }
-            return dt;
-        }
+				try
+				{
+					dt.ReadXml(stream);
+				}
+				catch (Exception)
+				{
+					throw new Exception(xml);
+				}
+			}
+			return dt;
+		}
 
 
-        public static string ToXml(this DataTable dt)
-        {
-            using (MemoryStream stream = new MemoryStream())
-            {
-                dt.WriteXml(stream, XmlWriteMode.WriteSchema);
-                stream.Flush();
-                stream.Position = 0;
+		public static string ToXml(this DataTable dt)
+		{
+			using (MemoryStream stream = new MemoryStream())
+			{
+				dt.WriteXml(stream, XmlWriteMode.WriteSchema);
+				stream.Flush();
+				stream.Position = 0;
 
-                using (StreamReader reader = new StreamReader(stream))
-                {
-                    return reader.ReadToEnd();
-                }
-            }
-        }
-    }
+				using (StreamReader reader = new StreamReader(stream))
+				{
+					return reader.ReadToEnd();
+				}
+			}
+		}
+	}
 }
