c5a03529f39d783d94d1a32362a333612c961fcb
F Jiang <fjiang2@github.com>
2021-09-05 11:37:18 -0500

    Created base lie of any-cli

---
+++ anycli/Shell/ShellContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sys.Data;
+using Sys.Stdio;
+using Sys.Stdio.Cli;
+
+namespace anycli
+{
+    class ShellContext
+    {
+        protected IApplicationConfiguration cfg { get; }
+        protected Commandee commandee { get; }
+
+        public ShellContext(IApplicationConfiguration cfg)
+        {
+            this.cfg = cfg;
+            this.commandee = new Commandee(cfg);
+        }
+
+        public void SwitchTask(IShellTask context)
+        {
+        }
+    }
+}
